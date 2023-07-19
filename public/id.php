c37d69b680a0fe5456f399793d0668ca2b201fb8
imran <imranhayredin89@gmail.com>
2023-07-19 09:39:23 +0300

    qr-scan

---
+++ public/id.php
@@ -0,0 +1,30 @@
+
+
+<?php
+
+// Get the value of the "id" parameter from the query string
+$id = isset($_GET['text']) ? $_GET['text'] : '';
+//header('Location: qr-scan.php');
+
+
+// Do something with the ID (e.g. store it in a database, look up information, etc.)
+// ...
+
+// Return a response
+
+
+?>
+
+<html>
+
+<script>
+
+
+//window.location.href = 'http://localhost/Gym/public/qr-scan.php';
+
+
+
+</script>
+
+
+</html>
